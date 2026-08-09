8b93f600ebf28e4b55d34310fa2b39452dea11a5
it33 <it33@users.noreply.github.com>
2026-08-09 14:27:23 -0400

    Add AI denim+marigold resource card images (press, blog, community)

---
+++ src/components/landing/resource-card-images.ts
@@ -0,0 +1,15 @@
+import press from "./resource-images/press";
+import blog from "./resource-images/blog";
+import community from "./resource-images/community";
+import careers from "./resource-images/careers";
+import partners from "./resource-images/partners";
+import sales from "./resource-images/sales";
+
+export const RESOURCE_CARD_IMAGES = {
+  press,
+  blog,
+  community,
+  careers,
+  partners,
+  sales,
+} as const;
